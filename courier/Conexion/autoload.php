c2109a9133d1e14ea4eaa5427c7f2bc40f68fa0c
VKevinxz <73483354+VKevinxz@users.noreply.github.com>
2021-05-07 07:54:58 -0500

    Delete courier directory

--- courier/Conexion/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-spl_autoload_register(function ($nombre_clase){
-    $extension = ".php";
-    $ruta = $nombre_clase.$extension;
-
-    if(file_exists($ruta)){
-        include_once $ruta;
-    }
-});
